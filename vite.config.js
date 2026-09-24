33ec6e6c9ffbd2e351f1a92be791b061a5c47061
vikash0482 <gujjaruvikash@gmail.com>
2026-09-24 23:30:30 +0530

    Allow Render hostname in Vite preview

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(
+  {
   plugins: [react()],
+  preview: {
+    
+    allowedHosts: ['keeper-app-60qh.onrender.com'],
+  },
 })
